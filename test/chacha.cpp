35e2127e53bb8fcca2a526560f1d6c65e2df912f
Masashi Fujita <objectxtreme@gmail.com>
2017-02-17 01:47:42 +0900

    refactor: Regroup tests

--- test/chacha.cpp
+++ test/chacha.cpp
@@ -35,49 +35,54 @@ namespace {
     }
 }
 
-TEST_CASE ("Encrypt empty bytes", "[chacha]") {
+TEST_CASE ("Test with small key", "[chacha]") {
     SECTION ("with small key") {
-        ECRYPT_ctx  ctx ;
-        memset (&ctx, 0, sizeof (ctx)) ;
+        ECRYPT_ctx ctx;
+        memset (&ctx, 0, sizeof (ctx));
         const std::string key { "0123456789abcdef" };
-        REQUIRE (8 * key.size () == 128) ;
-        ECRYPT_keysetup (&ctx, reinterpret_cast<const u8 *> (key.data ()), 128, 0) ;
-        std::array<uint8_t, 8>  iv ;
-        iv.fill (0) ;
-        ECRYPT_ivsetup (&ctx, static_cast<const u8 *> (iv.data ())) ;
-        ChaCha::State   S { key.data (), key.size (), 0 } ;
+        REQUIRE (8 * key.size () == 128);
+        ECRYPT_keysetup (&ctx, reinterpret_cast<const u8 *> (key.data ()), 128, 0);
+        std::array<uint8_t, 8> iv;
+        iv.fill (0);
+        ECRYPT_ivsetup (&ctx, static_cast<const u8 *> (iv.data ()));
+        ChaCha::State S { key.data (), key.size (), 0 };
 
-        auto const &    state = S.state () ;
-        for (int_fast32_t i = 0 ; i < state.size () ; ++i) {
-            CAPTURE (i) ;
-            REQUIRE (state [i] == ctx.input [i]) ;
+        SECTION ("Compare states") {
+            auto const        &state = S.state ();
+            for (int_fast32_t i = 0 ; i < state.size () ; ++i) {
+                CAPTURE (i);
+                REQUIRE (state[i] == ctx.input[i]);
+            }
         }
         SECTION ("Encrypt strings") {
             SECTION ("Encrypt \"A\"") {
-                auto const &    expected = encode (ctx, "A") ;
-                auto const &    actual = encode (S, "A") ;
-                REQUIRE (expected.size () == actual.size ()) ;
-                REQUIRE (expected == actual) ;
+                auto const &expected = encode (ctx, "A");
+                auto const &actual   = encode (S, "A");
+                REQUIRE (expected.size () == actual.size ());
+                REQUIRE (expected == actual);
             }
             SECTION ("Encrypt \"The ninja warrior are the immortal murder machines.\"") {
-                const std::string   src { "The ninja warrior are the immortal murder machines." };
-                auto const &    expected = encode (ctx, src) ;
-                auto const &    actual = encode (S, src) ;
-                REQUIRE (expected.size () == actual.size ()) ;
-                REQUIRE (expected == actual) ;
+                const std::string src { "The ninja warrior are the immortal murder machines." };
+                auto const &expected = encode (ctx, src);
+                auto const &actual   = encode (S, src);
+                REQUIRE (expected.size () == actual.size ());
+                REQUIRE (expected == actual);
             }
             SECTION ("Encrypt \"The ninja warrior are the immortal murder machines.\" (splitted)") {
-                const std::string   src { "The ninja warrior are the immortal murder machines." };
-                auto off = src.size () / 2 ;
-                auto const &    expected = encode (ctx, src) ;
-                auto &&    a0 = encode (S, src.substr (0, off), 0) ;
-                auto const &    a1 = encode (S, src.substr (off), off) ;
-                a0.insert (a0.end (), a1.cbegin (), a1.cend ()) ;
-                REQUIRE (expected.size () == a0.size ()) ;
-                REQUIRE (expected == a0) ;
+                const std::string src { "The ninja warrior are the immortal murder machines." };
+                auto              off = src.size () / 2;
+                auto const &expected = encode (ctx, src);
+                auto       &&a0      = encode (S, src.substr (0, off), 0);
+                auto const &a1       = encode (S, src.substr (off), off);
+                a0.insert (a0.end (), a1.cbegin (), a1.cend ());
+                REQUIRE (expected.size () == a0.size ());
+                REQUIRE (expected == a0);
             }
         }
     }
+}
+
+TEST_CASE ("Test with large key", "[chacha20]") {
     SECTION ("with large key") {
         ECRYPT_ctx  ctx ;
         memset (&ctx, 0, sizeof (ctx)) ;
@@ -89,10 +94,12 @@ TEST_CASE ("Encrypt empty bytes", "[chacha]") {
         ECRYPT_ivsetup (&ctx, static_cast<const u8 *> (iv.data ())) ;
         ChaCha::State   S { key.data (), key.size (), 0 } ;
 
-        auto const &    state = S.state () ;
-        for (int_fast32_t i = 0 ; i < state.size () ; ++i) {
-            CAPTURE (i) ;
-            REQUIRE (state [i] == ctx.input [i]) ;
+        SECTION ("Compare states") {
+            auto const &    state = S.state () ;
+            for (int_fast32_t i = 0 ; i < state.size () ; ++i) {
+                CAPTURE (i) ;
+                REQUIRE (state [i] == ctx.input [i]) ;
+            }
         }
         SECTION ("Encrypt strings") {
             SECTION ("Encrypt \"A\"") {
